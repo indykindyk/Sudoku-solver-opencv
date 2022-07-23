64b009fd45dea1c4c8a11b2963b57554b1e98729
Karol <bonollol130@gmail.com>
2022-07-23 22:57:24 +0200

    st

--- src/utlis.py
+++ src/utlis.py
@@ -6,9 +6,10 @@ from tensorflow.keras.models import load_model
 import cv2 as cv
 import images as im
 from scipy import ndimage as ndi
+import multiprocessing
 
 
-model = load_model('models/model-04-1.00.h5')	
+model = load_model('models/model-05-1.00.h5')	
 
 predictions = []
 
@@ -22,39 +23,27 @@ def split_photo(img):
             boxes.append(hs)
     return boxes
 
+def pref_in_parrel(img):
+    pre = im.preprocess_box(img)
+    pre = im.prepare_box(pre)
+
+    if pre.sum() >= 28**2*255 - 28 * 1 * 255:
+        return 0
+
+    box =  np.expand_dims(pre, axis=0)
+    box = box/255.
+    predict = model.predict(box)
+    digit = argmax(predict)
+    digit += 1
+    return digit
+
 # load an image and predict the class
-def predict(boxes):
-    # load model
-    x = 0
-    #give prediction for evry square
-    predictions = []
-    for img in boxes:
-        pre = im.preprocess_box(img)
-        name = f"box{x}.png"
-        pre = im.prepare_box(pre)
-        
-        x+=1
-
-        if pre.sum() >= 28**2*255 - 28 * 1 * 255:
-            predictions.append(0)
-            continue    # Move on if we have a white cell
-
-        center_width = pre.shape[1] // 2
-        center_height = pre.shape[0] // 2
-        x_start = center_height // 2
-        x_end = center_height // 2 + center_height
-        y_start = center_width // 2
-        y_end = center_width // 2 + center_width
-        center_region = pre[x_start:x_end, y_start:y_end]
-
-        box =  np.expand_dims(pre, axis=0)
-        box = box/255.
-        predict = model.predict(box)
-        digit = argmax(predict)
-        digit += 1
-        print(f"[{x}] pred: {digit}")
-        predictions.append(digit)
-
+def predict(boxes): 
+    #make prediction for evry square
+    print(boxes)
+    p = multiprocessing.Pool(4)
+    predictions = p.map(pref_in_parrel, boxes)
+    print(predictions)
     return np.array(predictions)
 
 def display_predictions(boxes, solved=False):
